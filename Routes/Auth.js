f652c996f7943cc7a6074ee78a261bcd796f50ba
aryallaxman99 <aryallaxman99@gmail.com>
2023-04-11 23:51:35 +0545

    bcrypt added

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 import User from "../Models/userModel.js";
 import authSchema from "../controllers/validationSchema.js";
 const router = express.Router();
@@ -11,6 +12,9 @@ router.post("/register", async (req, res, next) => {
     const doesEmailExist = await User.findOne({ email: result.email });
     if (doesEmailExist) throw createHttpError.Conflict("Email already exists");
 
+    const salt = bcrypt.genSaltSync(10); //saltRounds = 10
+    const hash = bcrypt.hashSync(result.password, salt);
+    req.body.password = hash;
     const savedUser = await User.create(req.body);
     savedUser
       ? console.log("new user registered")
