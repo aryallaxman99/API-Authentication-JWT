18d76b3dfb30dee5f04b2c34e29eb5d958c3bbbc
aryallaxman99 <aryallaxman99@gmail.com>
2023-04-12 16:38:47 +0545

    secret keys generated using crypto

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -5,7 +5,7 @@ const signAcessToken = (userId) => {
   return new Promise((resolve, reject) => {
     const payload = {};
 
-    const secret = "secret";
+    const secret = process.env.ACCESS_TOKEN_SECRET;
     const options = {
       expiresIn: "1h",
       issuer: "local",
